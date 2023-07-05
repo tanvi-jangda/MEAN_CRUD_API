d1459151a2a289689b8c52449458d69928b67ece
tanvi-jangda <58063235+tanvi-jangda@users.noreply.github.com>
2023-07-05 20:08:42 +0530

    Add files via upload

--- Routes/api.js
+++ Routes/api.js
@@ -1,12 +1,36 @@
 //internal imports
 const express = require('express');
+const jwt = require('jsonwebtoken');
 var app=express();
 const employeeRoute = express.Router();
 
 let Employee =require('../Models/Employee')
+let Login=require('../Models/Login')
+
+
+function verifyToken(req, res, next) 
+{
+  if(!req.headers.authorization) 
+  {
+    return res.status(401).send('Unauthorized request')
+  }
+  let token = req.headers.authorization.split(' ')[1]
+  if(token === 'null') 
+  {
+    return res.status(401).send('Unauthorized request')    
+  }
+  let payload = jwt.verify(token, 'secretKey')
+  if(!payload) 
+  {
+    return res.status(401).send('Unauthorized request')    
+  }
+  req.userId = payload.subject
+  next()
+}
+
 
 //get method
-employeeRoute.route('/GetEmployees').get(async (req,res)=>{
+employeeRoute.route('/GetEmployees',verifyToken).get(async (req,res)=>{
     await Employee.find({}).then((function(data){
         res.json(data);
       })
@@ -40,5 +64,22 @@ employeeRoute.route('/DeleteEmployee/:id').delete(async(req,res)=>{
     res.json(data);
   })
 })
+
+employeeRoute.route('/login').post(async(req,res)=>{
+ // console.log(req.body);
+  loginData=await Login.find({$and:[ {"Username":req.body.Username},  {"Password":req.body.Password}]});
+  //console.log(loginData);
+  if(Object.keys(loginData).length!=0)
+  {
+    let payload = {subject: loginData._id}
+    let token = jwt.sign(payload, 'secretKey')
+    res.status(200).send({token})   
+  } 
+  else 
+  {
+      res.status(401).send('Invalid Password')
+  } 
+})
+
 
 module.exports=employeeRoute;
